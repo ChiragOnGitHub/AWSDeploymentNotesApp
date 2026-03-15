7c4b3cd44bbfba8bf800750e6605396bacafdfba
ChiragOnGitHub <chirag.cs102@gmail.com>
2026-03-15 23:30:48 +0530

    bug fixed

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ app.delete("/api/notes/:id", async (req,res)=>{
 
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>console.log("Server running"))
+app.listen(PORT,()=>{
+ console.log("Server running on port "+PORT)
+})
